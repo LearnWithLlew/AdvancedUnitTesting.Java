6f19639c4661ff45d3412647ecba55e85568071f
isidore <llewellyn.falco@gmail.com>
2023-04-01 15:12:36 +0000

    t   Assume fixes
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- src/test/java/org/samples/DatabaseTests.java
+++ src/test/java/org/samples/DatabaseTests.java
@@ -11,64 +11,59 @@ import java.sql.*;
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class DatabaseTests
-{
-  @Test
-  void testConnection() throws SQLException {
-    skipTestOnCI();
-    assertEquals(42, SqlUtilites.executeNumericQuery("Select 42;", getConnection()));
-  }
-
-  private void skipTestOnCI() {
-    if (isCI()) {
-       assumeTrue(true);
+public class DatabaseTests {
+    @Test
+    void testConnection() throws SQLException {
+        skipTestOnCI();
+        assertEquals(42, SqlUtilites.executeNumericQuery("Select 42;", getConnection()));
     }
-  }
 
-  private boolean isCI() {
-    String[] environmentVariablesForCI = {
-            // begin-snippet: supported_ci_env_vars
-            "CI",
-            "CONTINUOUS_INTEGRATION",
-            "GITHUB_ACTIONS",
-            "GO_SERVER_URL",
-            "JENKINS_URL",
-            "TEAMCITY_VERSION",
-            "TF_BUILD"
-            // end-snippet
-    };
-
-    for (var variable : environmentVariablesForCI)
-    {
-      if (StringUtils.isNonZero(System.getenv(variable)))
-      {
-        return true;
-      }
+    private void skipTestOnCI() {
+        assumeFalse(isCI());
     }
-    return false;
-  }
 
+    private boolean isCI() {
+        String[] environmentVariablesForCI = {
+                // begin-snippet: supported_ci_env_vars
+                "CI",
+                "CONTINUOUS_INTEGRATION",
+                "GITHUB_ACTIONS",
+                "GO_SERVER_URL",
+                "JENKINS_URL",
+                "TEAMCITY_VERSION",
+                "TF_BUILD"
+                // end-snippet
+        };
 
-  @Test
-  void testVersion() throws SQLException {
-    skipTestOnCI();
-    assertEquals(1, SqlUtilites.executeNumericQuery("Select value from metadata where name='schema_version'", getConnection()));
-  }
-
-  @Test
-  void testSchema() throws SQLException {
-    skipTestOnCI();
-    var schema = SqlUtilites.loadSchema(getConnection());
-    Approvals.verify(SqlUtilites.print(schema));
-  }
-
-  private Connection getConnection()  {
-    try {
-      return  DriverManager.getConnection("jdbc:mariadb://localhost:3306/sakila", "root", "");
-    } catch (SQLException e) {
-      throw ObjectUtils.throwAsError(e);
+        for (var variable : environmentVariablesForCI) {
+            if (StringUtils.isNonZero(System.getenv(variable))) {
+                return true;
+            }
+        }
+        return false;
     }
-  }
+
+
+    @Test
+    void testVersion() throws SQLException {
+        skipTestOnCI();
+        assertEquals(1, SqlUtilites.executeNumericQuery("Select value from metadata where name='schema_version'", getConnection()));
+    }
+
+    @Test
+    void testSchema() throws SQLException {
+        skipTestOnCI();
+        var schema = SqlUtilites.loadSchema(getConnection());
+        Approvals.verify(SqlUtilites.print(schema));
+    }
+
+    private Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mariadb://localhost:3306/sakila", "root", "");
+        } catch (SQLException e) {
+            throw ObjectUtils.throwAsError(e);
+        }
+    }
 }
