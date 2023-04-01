dd1f16f9a2f20add407df9075f12b4c78a599fb6
isidore <llewellyn.falco@gmail.com>
2023-04-01 15:07:22 +0000

    t    skip on CI
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- src/test/java/org/samples/DatabaseTests.java
+++ src/test/java/org/samples/DatabaseTests.java
@@ -2,28 +2,64 @@ package org.samples;
 
 
 import com.spun.util.ObjectUtils;
+import com.spun.util.StringUtils;
 import org.approvaltests.Approvals;
+import org.approvaltests.namer.NamerFactory;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
 import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class DatabaseTests
 {
   @Test
   void testConnection() throws SQLException {
+    skipTestOnCI();
     assertEquals(42, SqlUtilites.executeNumericQuery("Select 42;", getConnection()));
   }
 
+  private void skipTestOnCI() {
+    if (isCI()) {
+       assumeTrue(true);
+    }
+  }
+
+  private boolean isCI() {
+    String[] environmentVariablesForCI = {
+            // begin-snippet: supported_ci_env_vars
+            "CI",
+            "CONTINUOUS_INTEGRATION",
+            "GITHUB_ACTIONS",
+            "GO_SERVER_URL",
+            "JENKINS_URL",
+            "TEAMCITY_VERSION",
+            "TF_BUILD"
+            // end-snippet
+    };
+
+    for (var variable : environmentVariablesForCI)
+    {
+      if (StringUtils.isNonZero(System.getenv(variable)))
+      {
+        return true;
+      }
+    }
+    return false;
+  }
+
+
   @Test
   void testVersion() throws SQLException {
+    skipTestOnCI();
     assertEquals(1, SqlUtilites.executeNumericQuery("Select value from metadata where name='schema_version'", getConnection()));
   }
 
   @Test
   void testSchema() throws SQLException {
+    skipTestOnCI();
     var schema = SqlUtilites.loadSchema(getConnection());
     Approvals.verify(SqlUtilites.print(schema));
   }
